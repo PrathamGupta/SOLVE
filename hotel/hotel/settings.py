90d9e365359ac6fa3d9eba462c5dc3ae36a4c136
Pratham Gupta <pratham18072@iiitd.ac.in>
2020-05-12 20:02:27 +0530

    Added App for hotels

--- hotel/hotel/settings.py
+++ hotel/hotel/settings.py
@@ -31,6 +31,7 @@ ALLOWED_HOSTS = []
 # Application definition
 
 INSTALLED_APPS = [
+    'hotels.apps.HotelsConfig',
     'django.contrib.admin',
     'django.contrib.auth',
     'django.contrib.contenttypes',
@@ -75,8 +76,11 @@ WSGI_APPLICATION = 'hotel.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
+        'ENGINE': 'django.db.backends.mysql',
+        'NAME': 'solve',
+        'USER': 'pratham',
+        'PASSWORD': 'Pratham@123',
+        'HOST': '127.0.0.1',
     }
 }
 
@@ -105,7 +109,7 @@ AUTH_PASSWORD_VALIDATORS = [
 
 LANGUAGE_CODE = 'en-us'
 
-TIME_ZONE = 'UTC'
+TIME_ZONE = 'Asia/Calcutta'
 
 USE_I18N = True
 
--- hotel/hotels/models.py
+++ hotel/hotels/models.py
@@ -4,3 +4,16 @@ from __future__ import unicode_literals
 from django.db import models
 
 # Create your models here.
+class Register(models.Model):
+    first_name = models.CharField(max_length=20)
+    last_name = models.CharField(max_length=20)
+    email = models.CharField(max_length=20)
+    password = models.CharField(max_length=20)
+    hotel = models.BooleanField(default=False)
+
+class Card(models.Model):
+    hotel_name = models.CharField(max_length=50)
+    city = models.CharField(max_length=20)
+    state = models.CharField(max_length=20)    
+    date = models.DateField()
+    price = models.IntegerField(default=0)
