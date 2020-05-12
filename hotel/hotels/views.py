ff07b41816fb445823d950e387403cff5277417e
Pratham Gupta <pratham18072@iiitd.ac.in>
2020-05-12 23:26:24 +0530

    added first frontend page

--- hotel/hotels/views.py
+++ hotel/hotels/views.py
@@ -3,7 +3,18 @@ from __future__ import unicode_literals
 
 from django.shortcuts import render
 from django.http import HttpResponse
+from django.template import loader
+from .models import Card
+
 
 # Create your views here.
 def index(request):
-    return HttpResponse("Hello, World. You are at the Hotel Cards.")
+    card_list= Card.objects.all()
+    template = loader.get_template('hotels/index.html')
+    context = {
+        'card_list' : card_list,
+    }
+    return render(request, 'hotels/index.html', context)
+
+def makeCard(request):
+    
